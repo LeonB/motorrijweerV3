808e993644d238641b7fba659d2252f4d94a62b0
Leon Bogaert <leon@tim-online.nl>
2013-11-26 00:14:41 +0100

    Some stuff?

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,11 +1,14 @@
 class Region < ActiveRecord::Base
   has_many :stations
-  belongs_to :region
+  belongs_to :parent, :class_name => Region, :foreign_key => :parent_id
   has_many :regions, :foreign_key => :parent_id, :dependent => :delete_all
   acts_as_nested_set
-  before_save :setup_code
+  before_validation { self.code = self.generate_code}
 
-  def setup_code
-    self.code = self.name.downcase.gsub(/-/, '')
+  # Auto generate the code value
+  # The code is based on <tt>name</tt> and stripped from all non-alphanumberic characters
+  # <tt>Code</tt> is used for slugs (?)
+  def generate_code
+    self.name.downcase.gsub(/[^\w]/, '')
   end
 end
