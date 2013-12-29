34d1355d64f0461d5f1cd7e55d3f8d831f72e0cc
Leon Bogaert <leon@tim-online.nl>
2013-12-29 14:41:20 +0100

    Rollback + migrate

--- db/schema.rb
+++ db/schema.rb
@@ -16,49 +16,52 @@ ActiveRecord::Schema.define(version: 20131113214459) do
   create_table "forecasts", force: true do |t|
     t.datetime "from_datetime"
     t.datetime "to_datetime"
+    t.string   "period"
     t.integer  "station_id"
     t.string   "provider"
     t.string   "description"
     t.string   "weather_type"
     t.string   "precipitation_type"
-    t.decimal  "precipitation_accumulation_in_centimers"
-    t.decimal  "precipitation_probability",               precision: 5, scale: 2
-    t.decimal  "precipitation_in_mm_per_hour_max",        precision: 5, scale: 4
-    t.decimal  "precipitation_in_mm_per_hour_min",        precision: 5, scale: 4
-    t.decimal  "precipitation_in_mm_per_hour_avg",        precision: 5, scale: 4
-    t.decimal  "winter_precipitation_in_mm_per_hour_max", precision: 5, scale: 4
-    t.decimal  "winter_precipitation_in_mm_per_hour_min", precision: 5, scale: 4
-    t.decimal  "winter_precipitation_in_mm_per_hour_avg", precision: 5, scale: 4
-    t.decimal  "temperature_in_celcius_max",              precision: 5, scale: 2
-    t.decimal  "temperature_in_celcius_min",              precision: 5, scale: 2
-    t.decimal  "temperature_in_celcius_avg",              precision: 5, scale: 2
-    t.decimal  "apparent_temperature_in_celcius_max",     precision: 5, scale: 2
-    t.decimal  "apparent_temperature_in_celcius_min",     precision: 5, scale: 2
-    t.decimal  "apparent_temperature_in_celcius_avg",     precision: 5, scale: 2
-    t.decimal  "wind_speed_in_meters_per_second_max",     precision: 5, scale: 2
-    t.decimal  "wind_speed_in_meters_per_second_min",     precision: 5, scale: 2
-    t.decimal  "wind_speed_in_meters_per_second_avg",     precision: 5, scale: 2
+    t.decimal  "precipitation_accumulation_in_centimers_max", precision: 5, scale: 4
+    t.decimal  "precipitation_accumulation_in_centimers_min", precision: 5, scale: 4
+    t.decimal  "precipitation_accumulation_in_centimers_avg", precision: 5, scale: 4
+    t.decimal  "precipitation_probability",                   precision: 5, scale: 2
+    t.decimal  "precipitation_in_mm_per_hour_max",            precision: 5, scale: 4
+    t.decimal  "precipitation_in_mm_per_hour_min",            precision: 5, scale: 4
+    t.decimal  "precipitation_in_mm_per_hour_avg",            precision: 5, scale: 4
+    t.decimal  "winter_precipitation_in_mm_per_hour_max",     precision: 5, scale: 4
+    t.decimal  "winter_precipitation_in_mm_per_hour_min",     precision: 5, scale: 4
+    t.decimal  "winter_precipitation_in_mm_per_hour_avg",     precision: 5, scale: 4
+    t.decimal  "temperature_in_celcius_max",                  precision: 5, scale: 2
+    t.decimal  "temperature_in_celcius_min",                  precision: 5, scale: 2
+    t.decimal  "temperature_in_celcius_avg",                  precision: 5, scale: 2
+    t.decimal  "apparent_temperature_in_celcius_max",         precision: 5, scale: 2
+    t.decimal  "apparent_temperature_in_celcius_min",         precision: 5, scale: 2
+    t.decimal  "apparent_temperature_in_celcius_avg",         precision: 5, scale: 2
+    t.decimal  "wind_speed_in_meters_per_second_max",         precision: 5, scale: 2
+    t.decimal  "wind_speed_in_meters_per_second_min",         precision: 5, scale: 2
+    t.decimal  "wind_speed_in_meters_per_second_avg",         precision: 5, scale: 2
     t.integer  "wind_bearing_in_degrees_max"
     t.integer  "wind_bearing_in_degrees_min"
     t.integer  "wind_bearing_in_degrees_avg"
-    t.decimal  "cloud_cover_max",                         precision: 5, scale: 2
-    t.decimal  "cloud_cover_min",                         precision: 5, scale: 2
-    t.decimal  "cloud_cover_avg",                         precision: 5, scale: 2
-    t.decimal  "humidity_max",                            precision: 5, scale: 2
-    t.decimal  "humidity_min",                            precision: 5, scale: 2
-    t.decimal  "humidity_avg",                            precision: 5, scale: 2
-    t.decimal  "pressure_in_millibars_max",               precision: 5, scale: 2
-    t.decimal  "pressure_in_millibars_min",               precision: 5, scale: 2
-    t.decimal  "pressure_in_millibars_avg",               precision: 5, scale: 2
-    t.decimal  "visibility_in_meters_max",                precision: 5, scale: 2
-    t.decimal  "visibility_in_meters_min",                precision: 5, scale: 2
-    t.decimal  "visibility_in_meters_avg",                precision: 5, scale: 2
-    t.decimal  "ozone_in_dobson_max",                     precision: 5, scale: 2
-    t.decimal  "ozone_in_dobson_min",                     precision: 5, scale: 2
-    t.decimal  "ozone_in_dobson_avg",                     precision: 5, scale: 2
-    t.decimal  "dew_point_in_celcius_max",                precision: 5, scale: 2
-    t.decimal  "dew_point_in_celcius_min",                precision: 5, scale: 2
-    t.decimal  "dew_point_in_celcius_avg",                precision: 5, scale: 2
+    t.decimal  "cloud_cover_max",                             precision: 5, scale: 2
+    t.decimal  "cloud_cover_min",                             precision: 5, scale: 2
+    t.decimal  "cloud_cover_avg",                             precision: 5, scale: 2
+    t.decimal  "humidity_max",                                precision: 5, scale: 2
+    t.decimal  "humidity_min",                                precision: 5, scale: 2
+    t.decimal  "humidity_avg",                                precision: 5, scale: 2
+    t.decimal  "pressure_in_millibars_max",                   precision: 5, scale: 2
+    t.decimal  "pressure_in_millibars_min",                   precision: 5, scale: 2
+    t.decimal  "pressure_in_millibars_avg",                   precision: 5, scale: 2
+    t.decimal  "visibility_in_meters_max",                    precision: 5, scale: 2
+    t.decimal  "visibility_in_meters_min",                    precision: 5, scale: 2
+    t.decimal  "visibility_in_meters_avg",                    precision: 5, scale: 2
+    t.decimal  "ozone_in_dobson_max",                         precision: 5, scale: 2
+    t.decimal  "ozone_in_dobson_min",                         precision: 5, scale: 2
+    t.decimal  "ozone_in_dobson_avg",                         precision: 5, scale: 2
+    t.decimal  "dew_point_in_celcius_max",                    precision: 5, scale: 2
+    t.decimal  "dew_point_in_celcius_min",                    precision: 5, scale: 2
+    t.decimal  "dew_point_in_celcius_avg",                    precision: 5, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -82,5 +85,7 @@ ActiveRecord::Schema.define(version: 20131113214459) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "stations", ["latitude", "longitude"], name: "index_stations_on_latitude_and_longitude"
 
 end
